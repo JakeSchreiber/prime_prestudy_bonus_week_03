cbdf60b6135b170514e19da7b1b59be2765b9022
JakeSchreiber <jake@jakeschreiber.com>
2015-09-21 13:33:19 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var c=prompt("What number would you like to use in the function?")
+
+function functionA(a){
+	return a*a;
+}
+
+function functionB(b){
+	return b * b;
+}
+
+function functionC(c){
+	return c*c*c;
+}
+
+alert(functionA(functionB(functionC(c))));
